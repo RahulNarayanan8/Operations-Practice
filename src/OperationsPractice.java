c2c6a102a8c591a131100d124d827602adb2ba24
Rahulgvsd <43002701+Rahulgvsd@users.noreply.github.com>
2018-10-01 08:32:35 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,24 @@
+
+public class OperationsPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		int number;
+		number = 3;
+		System.out.println("the initial number is: " + number);
+		number +=5;
+		System.out.println("Add five: " + number);
+		number /=2;
+		System.out.println("Divide by 2: " + number);
+		number *=4;
+		System.out.println("Multiply by four: " + number);
+		number -=6;
+		System.out.println("Subtract 6: " + number);
+		number %=2;
+		System.out.println("Modularly divide by 2: " + number);
+		
+		
+	}
+
+}
